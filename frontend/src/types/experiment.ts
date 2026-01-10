846a881b9c309d189838e9c067faea3338e86ce3
Antipositive <148214981+Ant1pozitive@users.noreply.github.com>
2026-01-10 20:23:46 +0300

    Delete frontend/src directory

--- frontend/src/types/experiment.ts
+++
@@ -1,8 +0,0 @@
-export interface ExperimentState {
-  steps: number;
-  loss: number[];
-  attention: number[][][];
-  readWeights: number[][];
-  writeWeights: number[][];
-  memorySlots: number[][][];
-}
